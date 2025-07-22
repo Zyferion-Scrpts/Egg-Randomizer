6659f773997616fa23ae22370cab5837c033fb8e
Zyferion-Scrpts <uzilia769@gmail.com>
2025-07-22 19:02:43 +0800

    Create GrowAGarden.lua

---
+++ GrowAGarden.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/ampKy0JHQEsx6frn/raw", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Renasaix/All-in-One-/refs/heads/main/Eggies.lua"))()
